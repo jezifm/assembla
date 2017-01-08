b70d16176264b5faf9969341bf8c2817c897fde0
Jezrael Arciaga <jez@imfreemobile.com>
2017-01-08 14:57:46 +0800

    Provide test step definition

---
+++ features/step-definitions/assembla-steps.el
@@ -0,0 +1,37 @@
+(Given "^I start assembla$"
+       (lambda ()
+	 (call-interactively 'assembla)))
+
+(Given "^I load the following:$"
+       (lambda (body)
+	 (eval (car (read-from-string body)))))
+
+
+(When "^I press \"\\([^\"]+\\)\"$"
+      (lambda (key)
+	(execute-kbd-macro key)))
+
+(Then "^I should be in assembla mode$"
+      (lambda ()
+	(cl-assert (equal major-mode 'assembla-mode))
+	(cl-assert (equal mode-name "Assembla"))))
+
+(Then "^I should be in buffer \"\\([^\"]+\\)\"$"
+      (lambda (arg)
+	(cl-assert (equal (buffer-name (current-buffer)) arg))))
+
+(Then "^the buffer should be read only$" ;
+      (lambda ()
+	(cl-assert buffer-read-only)))
+
+(Then "^the variable \"\\([^\"]+\\)\" should be undefined$"
+      (lambda (variable-name)
+	(cl-assert (not (boundp (intern variable-name))))))
+
+(Then "^the variable \"\\([^\"]+\\)\" should have value \"\\([^\"]+\\)\"$"
+      (lambda (variable-name value)
+	(cl-assert (equal (eval (intern variable-name)) value) value)))
+
+(Then "I should not be in assembla mode"
+      (lambda ()
+	(cl-assert (not (equal major-mode 'assembla-mode)))))
